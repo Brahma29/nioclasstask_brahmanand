8f58888769ab35d577c81c0eaae066cfbfe05d0d
Brahmanand Swami <124257410+Brahma21@users.noreply.github.com>
2023-03-04 10:35:10 +0530

    Font size changed for question card

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -4,7 +4,7 @@ import { MathJax } from 'better-react-mathjax';
 
 function QuestionCard({ questionNumber, questionContent }) {
   return (
-    <div className="max-w-5xl p-5 mt-4 mb-4 text-2xl bg-white shadow-lg shadow-gray-400 rounded-lg w-full lg:w-[64rem]">
+    <div className="max-w-5xl p-5 mt-4 mb-4 text-xl md:text-2xl bg-white shadow-lg shadow-gray-400 rounded-lg w-full lg:w-[64rem]">
       <div className="card bg-base-100">
         <div className="text-center card-body">
           <h2 className="mb-4 card-title">Question No : {questionNumber}</h2>
